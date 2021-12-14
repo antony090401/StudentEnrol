b8a395a5425e503dc1ae0704997cbee8f775d5f4
student <student@computing.localdomain>
2021-12-14 11:46:45 +0000

    added class domains and scaffold view

---
+++ enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,17 @@
+package com.enrol
+
+class Course {
+
+// Variable Declaration
+	String department
+	String courseTitle
+	String courseLeader
+	String courseCode
+	Date startDate
+	Date endDate
+	String description
+	int numberOfStudents
+	double tuitionFees
+    static constraints = {
+    }
+}
